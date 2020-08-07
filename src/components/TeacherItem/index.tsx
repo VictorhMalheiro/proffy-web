976043df66f29f597cb62b818c592da6bf1db8f8
Victor Hugo Malheiro <victorhugobarretos@gmail.com>
2020-08-07 15:12:36 -0300

    Proffy web - Aula 1

---
+++ src/components/TeacherItem/index.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+import whatsappIcon from "../../assets/images/icons/whatsapp.svg";
+
+import "./styles.css";
+// import { Container } from './styles';
+
+const TeacherItem: React.FC = () => {
+  return (
+    <article className="teacher-item">
+      <header>
+        <img
+          src="https://avatars3.githubusercontent.com/u/38863412?s=460&u=b0b676e7bd2dc04afa0e62341a4f5d29e572827b&v=4"
+          alt="Victor Hugo Malheiro"
+        />
+        <div>
+          <strong>Victor Hugo Malheiro</strong>
+          <span>Química</span>
+        </div>
+      </header>
+
+      <p>
+        Entusiasta das melhores tecnologias de química avançada.
+        <br />
+        <br />
+        Apaixonado por explodir coisas em laboratório e por mudar a vida das
+        pessoas através de experiências. Mais de 200.000 pessoas já passaram por
+        uma das minhas explosões.
+      </p>
+      <footer>
+        <p>
+          Preço/hora
+          <strong>R$ 80,00</strong>
+        </p>
+        <button type="button">
+          <img src={whatsappIcon} alt="Whatsapp" />
+          Entrar em contato
+        </button>
+      </footer>
+    </article>
+  );
+};
+
+export default TeacherItem;
