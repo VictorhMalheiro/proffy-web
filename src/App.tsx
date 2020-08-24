a315ef974e416734cda3234f3cbf9228a341594f
Victor Hugo Malheiro <victorhugobarretos@gmail.com>
2020-08-24 14:24:01 -0300

    feat: :rocket: Project completion
    Desenvolvimento das conexões com o professor, cadastro de professores, busca de professores entre outras coisas

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 
 import Routes from './routes';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.min.css';
 
 import './assets/styles/global.css';
 
 function App() {
   return (
-    <Routes />
+    <>
+      <Routes />
+      <ToastContainer position="bottom-center" />
+    </>
   );
 }
 
--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -3,39 +3,54 @@ import React from "react";
 import whatsappIcon from "../../assets/images/icons/whatsapp.svg";
 
 import "./styles.css";
-// import { Container } from './styles';
+import api from "../../server/api";
 
-const TeacherItem: React.FC = () => {
+export interface Teacher {
+    id: number;
+    subject: string;
+    cost: number;
+    name: string;
+    avatar: string;
+    whatsapp: string;
+    bio: string;
+}
+interface TeacherItemProps {
+  teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+
+  function createNewConnection () {
+    api.post('connections', {
+      user_id: teacher.id,
+    });
+  }
+
   return (
     <article className="teacher-item">
       <header>
         <img
-          src="https://avatars3.githubusercontent.com/u/38863412?s=460&u=b0b676e7bd2dc04afa0e62341a4f5d29e572827b&v=4"
-          alt="Victor Hugo Malheiro"
+          src={teacher.avatar}
+          alt={teacher.name}
         />
         <div>
-          <strong>Victor Hugo Malheiro</strong>
-          <span>Química</span>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
         </div>
       </header>
 
       <p>
-        Entusiasta das melhores tecnologias de química avançada.
-        <br />
-        <br />
-        Apaixonado por explodir coisas em laboratório e por mudar a vida das
-        pessoas através de experiências. Mais de 200.000 pessoas já passaram por
-        uma das minhas explosões.
+        {teacher.bio}
       </p>
       <footer>
         <p>
           Preço/hora
-          <strong>R$ 80,00</strong>
+          <strong>R$ {teacher.cost}</strong>
         </p>
-        <button type="button">
+        <a onClick={createNewConnection} target="_blank" rel="noopener noreferrer" href={`https://wa.me/${teacher.whatsapp}?text=Gostaria%20de%20saber%20mais%20sobre%20as%20aulas%20|%20Proffy`} >
           <img src={whatsappIcon} alt="Whatsapp" />
           Entrar em contato
-        </button>
+        </a>
       </footer>
     </article>
   );
